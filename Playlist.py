b31d6b000cecb8cc00340994404ece89af8cd2d4
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-04-13 22:24:06 -0700

    added to song.py

--- Playlist.py
+++ Playlist.py
@@ -9,7 +9,9 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
+    new_song =Song(title)
+    new_song.set_next_song(self.__first_song)
+    self.__first_song = new_song
 
 
 
@@ -17,20 +19,40 @@ class Playlist:
   # The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index. Otherwise, return -1.
 
   def find_song(self, title):
-    pass
+    current_song = self.__first_song
+    index = 0
+
+    while current_song != None:
+        if current_song.get_title()== title:
+            return index
+        if current_song.get_title() != title:
+            current_song = current_song.get_next_song()
+            return index
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    pass
+    this_song = self.__first_song
+    prev_song = None
+
+    while this_song != None:
+        if this_song.get_title() != title:
+            previous_song = current_song
+            current_song = current_song.get_next_song()
 
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    pass
+    current_song = self.__first_song
+    counter = 0
+
+    while current_song != None:
+        counter += 1
+        current_song = current_song.get_next_song()
+    return counter
 
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
@@ -41,4 +63,10 @@ class Playlist:
   # 3. Song Title 3
 
   def print_songs(self):
-    pass
+    current_song = self.__first_song
+    counter = 1
+
+    while current_song != None:
+      print(f'{counter}- {current_song}')
+      current_song = current_song.get_next_song()
+      counter +=1
